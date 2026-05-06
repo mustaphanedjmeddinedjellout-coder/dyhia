f76ad9920d06a6e9fa45ba1875fa644a00e86658
nedjme055 <146088529+nedjme055@users.noreply.github.com>
2026-05-06 18:04:09 +0100

    Add Meta Pixel

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import "./globals.css";
 import type { Metadata, Viewport } from "next";
 import { Cairo, Tajawal } from "next/font/google";
+import Script from "next/script";
 
 const cairo = Cairo({
   subsets: ["arabic"],
@@ -48,6 +49,27 @@ export default function RootLayout({
       <body
         className={`${cairo.variable} ${tajawal.variable} font-[family-name:var(--font-cairo)] text-base bg-sand`}
       >
+        <Script id="meta-pixel" strategy="afterInteractive">
+          {`!function(f,b,e,v,n,t,s)
+{if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+n.queue=[];t=b.createElement(e);t.async=!0;
+t.src=v;s=b.getElementsByTagName(e)[0];
+s.parentNode.insertBefore(t,s)}(window, document,'script',
+'https://connect.facebook.net/en_US/fbevents.js');
+fbq('init', '1446260090874583');
+fbq('track', 'PageView');`}
+        </Script>
+        <noscript>
+          <img
+            height="1"
+            width="1"
+            style={{ display: "none" }}
+            src="https://www.facebook.com/tr?id=1446260090874583&ev=PageView&noscript=1"
+            alt=""
+          />
+        </noscript>
         {children}
       </body>
     </html>
